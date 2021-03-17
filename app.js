3540160b71e2bd9d2573ccac1a046e1ef66c1489
La Ode Safar <laodesafar94@gmail.com>
2021-03-17 14:00:08 +0700

    belajar buat server node js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require("http");
 
+const hostname = "127.0.0.1";
+const port = "3001";
+
 const server = http.createServer((req, res) => {
-  console.log(req);
+  req.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hai, Selamat belajar Node Js lewat Dokumentasi");
 });
 
-server.listen(3001);
+server.listen(port, hostname, () => {
+  console.log(`Server berjalan pada http://${hostname}:${port}`);
+});
