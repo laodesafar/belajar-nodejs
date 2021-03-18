9ca2cc38e800a9ee69b942f73051679e9b63a994
La Ode Safar <laodesafar94@gmail.com>
2021-03-18 14:14:01 +0700

    belajar pengenalan node js

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 const http = require("http");
 
-const hostname = "127.0.0.1";
-const port = "3001";
-
 const server = http.createServer((req, res) => {
-  req.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hai, Selamat belajar Node Js lewat Dokumentasi");
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Latihan 1</title></head>");
+    res.write(
+      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Submit</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/user") {
+    res.write("<html>");
+    res.write("<head><title>Latihan 1</title></head>");
+    res.write("<body><ul><li>User 1</li><li>User 2</li></ul></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/create-user" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const username = parsedBody.split("=")[1];
+      console.log(username);
+    });
+    req.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server berjalan pada http://${hostname}:${port}`);
-});
+server.listen(3000);
