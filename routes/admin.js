d8ac59bd4b3ef9550e3d51fc8f161299da8f0f22
La Ode Safar <laodesafar94@gmail.com>
2021-03-23 16:17:57 +0700

    menambahakan view html dan routenya

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,15 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
-// admin/add-produc => GET
+// admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Tambah Produk</button></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
-// admin/add-produc => POST
+// admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
