d39a00098341b9f90a6c745a508df35beeb9aa33
La Ode Safar <laodesafar94@gmail.com>
2021-03-24 11:10:16 +0700

    Latihan middleware dengan Express JS

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const path = require("path");
 
 const express = require("express");
 
+const rootDir = require("./utils/path");
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, "public")));
 
 app.use("/admin", adminRoutes);
 
 app.use(shopRoutes);
 
 app.use(function (req, res, next) {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(3000);
