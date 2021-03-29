4b182557dc804ea5f9c0cd1544744e9477802341
La Ode Safar <laodesafar94@gmail.com>
2021-03-29 13:20:50 +0700

    membuat controller MVC Node Js

--- app.js
+++ app.js
@@ -4,23 +4,22 @@ const express = require("express");
 
 const rootDir = require("./utils/path");
 
-const adminData = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const errorControler = require("./controllers/error");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, "public")));
 
-app.use("/admin", adminData.routes);
-
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use(function (req, res, next) {
-  res.status(404).render("404", { docTitle: "Page Not Found", path: "" });
-});
+app.use(errorControler.get404);
 
 app.listen(3000);
