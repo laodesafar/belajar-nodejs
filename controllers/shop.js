7cd20d1532deb46793f6fa624fe875c8d4566782
La Ode Safar <laodesafar94@gmail.com>
2021-03-31 15:48:21 +0700

    add shop cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,11 +1,23 @@
 const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/product-list", {
       prods: products,
-      docTitle: "Shop",
-      path: "/",
+      pageTitle: "All Products",
+      path: "/products",
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
     });
   });
 };
@@ -14,22 +26,37 @@ exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/index", {
       prods: products,
-      docTitle: "Shop",
+      pageTitle: "Shop",
       path: "/",
     });
   });
-}
+};
 
 exports.getCart = (req, res, next) => {
   res.render("shop/cart", {
     path: "/cart",
-    docTitle: "Your Cart"
-  })
-}
+    pageTitle: "Your Cart",
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart");
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
+  });
+};
 
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     path: "/checkout",
-    docTitle: "Checkout"
-  })
-}
+    pageTitle: "Checkout",
+  });
+};
