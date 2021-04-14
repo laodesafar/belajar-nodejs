e026a932ab721ee183da8c39aa0cc800c7b1b40d
La Ode Safar <laodesafar94@gmail.com>
2021-04-14 15:37:57 +0700

    path

--- utils/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("node-complete", "root", "dikdasmen310", {
-  dialect: "mysql",
-  host: "localhost",
-});
-
-module.exports = sequelize;
